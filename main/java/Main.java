c182f2424cf99ae06a7a906af87382a508d36ed7
Arsani Abdou <ic20b098@technikum-wien.at>
2021-12-28 18:27:46 +0000

    Replace Main.java

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,9 +1,9 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,113 +15,57 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.stage.*;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-public class Main extends Application {
+
+public class Main extends Application{
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage){
+/*
+        primaryStage.setTitle("Share_IT");
+        primaryStage.centerOnScreen();
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(300);
+
+        Image image = new Image(getClass().getResourceAsStream("logo.png"));
+        ImageView logoview = new ImageView(image);
+
+        TextField ipAndPortTextfield = new TextField();
+        Label label = new Label("IP and Port:");
+
+        Button connectButton = new Button("Connect");
+        connectButton.setPrefWidth(120);
+
+*/
+/*        logoview.setX(50);
+        logoview.setY(25);
+        logoview.setFitHeight(100);
+        logoview.setFitWidth(100);
+        logoview.setPreserveRatio(true);*//*
+
+
         VBox vbox = new VBox();
-        HBox hbox1 = new HBox();
-        HBox hbox2 = new HBox();
-        //ObservableList<String> listFolders = FXCollections.observableArrayList();
-        ListView<String> listFolders = new ListView<>();
+        vbox.setAlignment(Pos.CENTER);
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
 
-        TextField uploadFrom = new TextField();
-        TextField downloadFrom = new TextField();
-        TextField uploadTo = new TextField();
-        TextField downloadTo = new TextField();
-
-        Label label1 = new Label("to");
-        Label label2 = new Label("to");
-
-        Button btn_up = new Button("Upload");
-        Button btn_down = new Button("Download");
-
-        Button selectFileUp = new Button();
-        Button selectFolderUp = new Button();
-        Button selectFileDown = new Button();
-        Button selectFolderDown = new Button();
-        Button [] buttons = {selectFileUp, selectFolderUp, selectFileDown, selectFolderDown};
-
-        Image image = new Image(getClass().getResourceAsStream("f.png"));
-        ImageView view1 = new ImageView(image);
-        ImageView view2 = new ImageView(image);
-        ImageView view3 = new ImageView(image);
-        ImageView view4 = new ImageView(image);
-        ImageView [] views = {view1, view2, view3, view4};
-
-        for(ImageView imageView : views){
-            imageView.setFitHeight(20);
-            imageView.setFitWidth(20);
-        }
-
-        int i = 0;
-        for(Button button : buttons){
-            button.setGraphic(views[i]);
-            i++;
-        }
-
-        btn_down.setPrefWidth(120);
-        btn_up.setPrefWidth(120);
-
-        vbox.getChildren().addAll(listFolders, hbox1, hbox2);
-        hbox1.getChildren().addAll(btn_up, uploadFrom, selectFileUp, label1, uploadTo, selectFolderUp);
-        hbox2.getChildren().addAll(btn_down, downloadFrom, selectFileDown, label2, downloadTo, selectFolderDown);
         vbox.setSpacing(10);
-        hbox1.setSpacing(10);
-        hbox2.setSpacing(10);
-
+        hbox.setSpacing(10);
         vbox.setPadding(new Insets(10));
-        hbox1.setPadding(new Insets(10));
-        hbox2.setPadding(new Insets(10));
+        hbox.setPadding(new Insets(10));
 
-        EventHandler<ActionEvent> popFileWindow = new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                FileChooser fileChooser = new FileChooser();
-                File f = fileChooser.showOpenDialog(null);
-
-                if(event.getSource() == selectFileUp){
-                    uploadFrom.setText(f.getAbsolutePath());
-                }
-                else if(event.getSource() == selectFileDown){
-                    downloadFrom.setText(f.getAbsolutePath());
-                }
-                else{
-                    //Exceptionhandling
-                }
-            }
-        };
-
-        EventHandler<ActionEvent> popFolderWindow = new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                DirectoryChooser directoryChooser = new DirectoryChooser();
-                File d = directoryChooser.showDialog(null);
-
-                if(event.getSource() == selectFolderDown){
-                    downloadTo.setText(d.getAbsolutePath());
-                }
-                else if(event.getSource() == selectFolderUp){
-                    uploadTo.setText(d.getAbsolutePath());
-                }
-
-            }
-        };
-
-        selectFileUp.setOnAction(popFileWindow);
-        selectFolderUp.setOnAction(popFolderWindow);
-        selectFileDown.setOnAction(popFileWindow);
-        selectFolderDown.setOnAction(popFolderWindow);
-
-
-        Scene scene = new Scene(vbox, 300, 275);
-        primaryStage.setScene(scene);
+        vbox.getChildren().addAll(logoview, hbox, connectButton);
+        hbox.getChildren().addAll(label, ipAndPortTextfield);
+        Scene loginScene = new Scene(vbox, 300, 300);
+        primaryStage.setScene(loginScene);
         primaryStage.show();
+*/
+
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
